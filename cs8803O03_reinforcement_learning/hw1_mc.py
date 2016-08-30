3f53ee6d6797892963502b82af365431ff62969f
rkekatpure <rohan_kekatpure@intuit.com>
2016-08-30 15:51:53 -0700

    completed homework1

--- cs8803O03_reinforcement_learning/hw1_mc.py
+++ cs8803O03_reinforcement_learning/hw1_mc.py
@@ -1,38 +1,52 @@
 import random as rd
 import numpy as np
+from matplotlib import rc
+import matplotlib.pylab as pl
+
+# rc("text", usetex=True)
 
 def roll(N_):
-	return rd.choice(range(1, N_ + 1))
+  return rd.choice(range(1, N_ + 1))
 
-def game(B_, max_rounds):
-	# Roll the dice, get the value `n` of the face. 
-	# If bit number `n` is 1 in B_, then end the game, else
-	# add `n` to total earnings.
-	max_sides = len(B)
-	nr = 0	
-	amounts = np.zeros((1, max_rounds), dtype=np.float)
-	terminate = False
-	while (nr < max_rounds) and (terminate is False):
-		side = roll(max_sides)		
-		if B_[side - 1] == 0:
-			amounts[0, nr] = amounts[0, nr - 1] + side
-		else:
-			amounts[0, nr] = 0.0
-			terminate = True
-		nr += 1
-	return amounts
+def is_bad(n, B_):
+  return B_[n - 1] == 1
+
+def game(B_, quit_amt):
+  max_sides = len(B_) 
+  tot_amt = 0
+  while tot_amt <= quit_amt:
+    a = roll(max_sides)
+    if is_bad(a, B_):
+      return 0
+    tot_amt += a
+  return tot_amt
 
 if __name__ == '__main__':
-	B = [int(i) for i in "10101110"]
-	max_games = 10000
-	max_rounds = 5
-	exp_amounts = np.zeros((1, max_rounds))
+  DIEN = "0111001100111011001011111001"
+  MAX_GAMES = 10000000
+  B = [int(i) for i in DIEN]  
+  quitting_amounts = range(0, 15, 1)
+  earnings = np.zeros((MAX_GAMES, len(quitting_amounts)))
+  for idx, qamt in enumerate(quitting_amounts):
+    exp_ern = []
+    print "quitting amount = $%s" % qamt
+    for _ in range(MAX_GAMES):            
+      exp_ern.append(game(B, qamt))   
+    earnings[:, idx] = exp_ern  
+  
+  earnings_mean = np.mean(earnings, axis=0)
+  earnings_sd = 2.6 * np.std(earnings, axis=0) / np.sqrt(MAX_GAMES)
 
-	for _ in range(max_games):
-		exp_amounts += game(B, max_rounds) / float(max_games)
-
-print exp_amounts
-
-
+  pl.figure(figsize=(8, 8))
+  pl.errorbar(quitting_amounts, earnings_mean, yerr=earnings_sd, 
+              fmt="o", ecolor="b")
+  pl.xlim([-2, max(quitting_amounts) + 1])
+  pl.xlabel("quitting amount (USD)")
+  pl.ylabel("expected earnings (USD)")
+  ymin, ymax = min(earnings_mean) - 0.1, max(earnings_mean) + 0.1
+  pl.ylim([ymin, ymax])
+  pl.text(20, 0.9 * ymax, "B = {%s}" % DIEN, fontsize=9)
+  pl.show()
+  # pl.savefig("die_%s.png" % DIEN, bbox_inches="tight")   
 
 
---
+++ cs8803O03_reinforcement_learning/hw1_value_iteration.py
@@ -0,0 +1,15 @@
+def value(n, B_, MAX_AMT_):
+	if n >= MAX_AMT_:
+		return n
+
+	vn = 0
+	for i, b in enumerate(B_):
+		if b == 0:
+			vn += value(n + i + 1, B_, MAX_AMT_)
+	return max(n, vn / float(len(B_)))
+
+if __name__ == "__main__":
+	DICE = "10101110"
+	B = [int(f) for f in DICE]
+	MAX_AMT = 25
+	print "B = {%s}\nvalue = %f" % (B, value(0, B, MAX_AMT))
